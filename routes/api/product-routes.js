9ae846701f6225cf2e87b37725b1d866ac2496cc
elReclusa <howell_j@hotmail.com>
2023-01-18 21:18:21 -0600

    Added most of the routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { DEC8_BIN } = require('mysql2/lib/constants/charsets');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
@@ -7,13 +8,60 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  Product.findAll({
+    attributes: ['id','product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dBProductData => res.json(dBProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include : [
+      {
+        model:Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id','tag_name']
+      }
+    ]
+    })
+  })
+  .then(dBProductData => {
+    if(!dBProductData) {
+      res.status(400).json( {message: 'This ID does not match a product!'});
+      return;
+    }
+    res.json(dBProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 
 // create new product
 router.post('/', (req, res) => {
@@ -25,9 +73,17 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+
+
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -91,6 +147,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
+  Product.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then(dBProductData => {
+    if(!dBProductData) {
+      res.status(404).json( {message: 'This ID does not match a product!'});
+      return;
+    }
+    res.json(dBProductData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
